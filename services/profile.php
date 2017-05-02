fc442eb5f663cda0c4b7f138625fe7e0454393e1
danielleklaasen <danielleklaasen@gmail.com>
2017-05-02 16:31:27 +0200

    profile and open results

--- services/profile.php
+++ services/profile.php
@@ -1,7 +1,7 @@
 <!-- PROFILE WINDOW -->
 <div class="wdw wdw-profile base-color-bg">
     <div class="container">
-        <div class="space"></div>
-        <p>Profile</p>
+        <h1>My <span class="bold"> profile</span></h1>
+        <button class="btn-1">Log out</button>
     </div>
 </div>
