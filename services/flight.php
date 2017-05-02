13da5fa2aea58759abc9bf77cee40c94d2844dcb
Bogdan Bele <bogdan.bele@yahoo.com>
2017-05-02 12:20:27 +0200

    Logo update

--- services/flight.php
+++ services/flight.php
@@ -1,7 +1,9 @@
 <!-- FLIGHT WINDOW -->
-<div class="wdw wdw-home base-color-bg">
+<div class="wdw wdw-flight base-color-bg open">
     <div class="container">
         <div class="space"></div>
         <p>home</p>
+        <input id="destination" class="floating-label" placeholder="Type Destination"></input>
+        <label for="name" class="active">Name</label>
     </div>
 </div>
