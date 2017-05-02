ae5c51099db05e589174953caac5608581628b39
Bogdan Bele <bogdan.bele@yahoo.com>
2017-05-02 15:38:22 +0200

    Styling the Flights page

--- services/flight.php
+++ services/flight.php
@@ -2,7 +2,16 @@
 <div class="wdw wdw-flight base-color-bg open">
     <div class="container">
         <div id="searchBar">
-        <input class="" placeholder="destination" required></div></div>
+        <input class="" placeholder="destination" required></div>
+        <div id="flightResults">
+          <div id="departingFrom">
+          </div>
+          <div id="departingTo">
+          </div>
+          <div class="flightResult"> </div>
+        </div>
+
+      </div>
         <p>home</p>
     </div>
 </div>
