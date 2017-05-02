87c4765b34a7e26bd94af654c656ce53b268999d
Bogdan Bele <bogdan.bele@yahoo.com>
2017-05-02 19:24:19 +0200

    The biggest commit yet

--- services/results.php
+++ services/results.php
@@ -2,18 +2,21 @@
 <div class="wdw wdw-results base-color-bg">
 
   <div id="flight-results">
+    <div class="flights">
     <div class="flights-information">
         <div class="companies-used">
           Ryanair
         </div>
         <div class="flights-timeline">
           <div class="company-logo">
-            <img class="image-logo" src="img/plane1.png" />
+            <img class="image-logo" src="img/plane5.png" />
           </div>
           <div class="departing-from">
             <div class="departing-time">
+              <span class="departure"></span><span class="time">  07:00 </span>
             </div>
             <div class="departing-location">
+                <span class="departure-long">Copenhagen</span>
             </div>
           </div>
           <div class="flight-duration">
@@ -26,10 +29,53 @@
             <div class="arrival-time">
             </div>
             <div class="arrival-location">
+
             </div>
           </div>
       </div>
     </div>
+
+
+    <div class="flights-information">
+        <div class="companies-used">
+          Wizzair
+        </div>
+        <div class="flights-timeline">
+          <div class="company-logo">
+            <img class="image-logo" src="img/plane5.png" />
+          </div>
+          <div class="departing-from">
+            <div class="departing-time">
+              <span class="departure"></span><span class="time">  07:00 </span>
+            </div>
+            <div class="departing-location">
+                <span class="departure-long">Copenhagen</span>
+            </div>
+          </div>
+          <div class="flight-duration">
+            <div class="duration">
+              <span> 3h 28m </span>
+            </div>
+
+            <svg height="8px" width="50" class="lineSVG">
+  <line x1="0" y1="0" x2="200" y2="0" style="" />
+  Sorry, your browser does not support inline SVG.
+            </svg>
+
+            <div class="frequency">
+                <span>    1 STOP </span>
+            </div>
+          </div>
+          <div class="destination">
+            <div class="arrival-time">
+            </div>
+            <div class="arrival-location">
+
+            </div>
+          </div>
+      </div>
+    </div>
+</div>
     <div class="purchase-ticket">
       <div class="ticket-information">
       </div>
