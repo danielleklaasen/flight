07c4a2cf73f94e6cc9e84d2cfb39dac9495c9e9c
danielleklaasen <danielleklaasen@gmail.com>
2017-05-02 12:20:50 +0200

    mellow mood

--- services/navigation.php
+++ services/navigation.php
@@ -1,5 +1,11 @@
 <nav class="main">
-<a id="wdw-template" href="javascript:void(0)" class="btn-wdw">Template</a>
+    <div class="container">
+        <div class="menu-burger">Menu</div>
+        <img class="logo" src="assets/logo.svg" alt />
+        <div class="profile">Profile</div>
+    </div>
+
+<!--<a id="wdw-template" href="javascript:void(0)" class="btn-wdw">Template</a>
 <a id="wdw-flight" href="javascript:void(0)" class="btn-wdw">Home</a>
-<a id="wdw-profile" href="javascript:void(0)" class="btn-wdw">Profile</a>
+<a id="wdw-profile" href="javascript:void(0)" class="btn-wdw">Profile</a>-->
 </nav>
