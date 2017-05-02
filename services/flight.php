f6922ba737c42cf423eb48843402608dbce987ea
Bogdan Bele <bogdan.bele@yahoo.com>
2017-05-02 13:30:17 +0200

    Input Field

--- services/flight.php
+++ services/flight.php
@@ -1,7 +1,8 @@
 <!-- FLIGHT WINDOW -->
-<div class="wdw wdw-flight base-color-bg">
+<div class="wdw wdw-flight base-color-bg open">
     <div class="container">
         <div class="space"></div>
+        <input class="" placeholder="destination" required></div>
         <p>home</p>
     </div>
 </div>
