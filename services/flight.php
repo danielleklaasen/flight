2224de4cdf135f3246a65ca9d37a9e3d760dca08
danielleklaasen <danielleklaasen@gmail.com>
2017-05-02 13:32:23 +0200

    solving conflicts 2

--- services/flight.php
+++ services/flight.php
@@ -2,6 +2,7 @@
 <div class="wdw wdw-flight base-color-bg open">
     <div class="container">
         <div class="space"></div>
+        <input class="" placeholder="destination" required></div>
         <p>home</p>
     </div>
 </div>
