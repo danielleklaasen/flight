d51bf7ef6edd9d7c0668955ff13390c57e1a2bfc
danielleklaasen <danielleklaasen@gmail.com>
2017-05-02 17:06:44 +0200

    login register buttons

--- services/header.php
+++ services/header.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Eczar:400,700|Work+Sans:300,400,500" rel="stylesheet">
     <title>Flight Search</title>
--- services/navigation.php
+++ services/navigation.php
@@ -18,6 +18,11 @@
     <div class="btn-close-menu"></div>
     <a id="wdw-template" href="javascript:void(0)" class="btn-wdw menu-item link-disappear-primary">Template</a>
     <a id="wdw-flight" href="javascript:void(0)" class="btn-wdw menu-item link-disappear-primary">Home</a>
+    <a id="wdw-results" href="javascript:void(0)" class="btn-wdw menu-item link-disappear-primary">Results</a>
     <a id="wdw-profile" href="javascript:void(0)" class="btn-wdw menu-item link-disappear-primary">Profile</a>
-      <a id="wdw-results" href="javascript:void(0)" class="btn-wdw menu-item link-disappear-primary">Results</a>
+    <div class="button-container">
+        <button id="wdw-login"class="btn-wdw menu-item btn-2">Login</button>
+        <button id="wdw-register" class="btn-wdw menu-item sub-item btn btn-2">Register</button>
+    </div>
+
 </div>
