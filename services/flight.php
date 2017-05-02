9832b587e762de3b4a8abac356268990ffe6fac2
danielleklaasen <danielleklaasen@gmail.com>
2017-05-02 15:59:41 +0200

    landing page adding elements

--- services/flight.php
+++ services/flight.php
@@ -1,6 +1,16 @@
 <!-- FLIGHT WINDOW -->
 <div class="wdw wdw-flight base-color-bg open">
+
+    <section class="horizontal-vertical-center box page-landing highlight-color-bg secondary-color">
+        <h3>Fly light</h3>
+        <h1>We stress, you <span class="bold">relax</span></h1>
+        <a href="#searchBar" class="link-disappear-base">Search flights now</a>
+    </section>
+
     <div class="container">
+
+
+
         <div id="searchBar">
         <input class="" placeholder="destination" required></div>
         <div id="flightResults">
@@ -11,7 +21,22 @@
           <div class="flightResult"> </div>
         </div>
 
+        <section class="horizontal-vertical-center box page-account secondary-color-bg">
+            <h6>ACCOUNT / INFORMATION</h6>
+            <h1>Why you should take an account</h1>
+            <p>Tumblr stumptown marfa, slow-carb williamsburg tbh retro woke direct trade readymade cold-pressed pinterest twee. Twee shoreditch cornhole distillery la croix. Fingerstache stumptown hoodie pabst, paleo actually retro 3 wolf moon squid tote bag fixie. Tofu migas marfa subway tile, venmo waistcoat thundercats fanny pack retro jean shorts. Yr gochujang before they sold out succulents flexitarian, pour-over subway tile chia blue bottle. Meditation copper mug sartorial chillwave, food truck banh mi poke kinfolk XOXO. Tumblr flannel snackwave, tote bag unicorn try-hard food truck yr.</p>
+            <hr>
+            <p class="note"><span class="attention">Personal suggestions</span> for business trips based on your booking history. We stress, you relax.</p>
+        </section>
+
       </div>
-        <p>home</p>
+
+
+
+    <?php
+    //INSERT FOOTER
+    require_once('footer.php');
+    ?>
+
     </div>
 </div>
